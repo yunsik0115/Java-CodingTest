fd4b9b62788e71b278d7b510fbbc20f5f77a8892
Yunsik Kim <yunsik0115@uos.ac.kr>
2023-10-27 11:22:05 +0900

    Baekjoon 24047 failed & Baekjoon 23881 Succeess

--- src/Baekjoon/Algorithm_Class/BubbleSort_4_24047.java
+++ src/Baekjoon/Algorithm_Class/BubbleSort_4_24047.java
@@ -10,6 +10,7 @@ public class BubbleSort_4_24047 {
 
     static int[] arr;
     static int swapNo = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -31,22 +32,26 @@ public class BubbleSort_4_24047 {
         int remainder = n % 5;
         boolean indicator = false;
 
-        if(quotient == 0) {  // 몫이 0인 경우 그냥 깡으로 bubbleSort 전범위 적용
-            indicator = bubbleSort(arr, 0, remainder, n, k, bw);
-        } else{
-            for(turn = 0; turn < quotient; turn++){ // 몫이 0이 아닌 경우 범위별로 bubblesort 실행
+        if (quotient == 0) {  // 몫이 0인 경우 그냥 깡으로 bubbleSort 전범위 적용
+            indicator = bubbleSort(arr, 0, remainder, n, k);
+        } else {
+            for (turn = 0; turn < quotient; turn++) { // 몫이 0이 아닌 경우 범위별로 bubblesort 실행
                 // 몫만큼 반복, 마지막 시행은 remainder만큼만 실행
-                if(indicator) break; // 시행이 끝나기 이전에 swapNo == k 만족시 true를 반환하기 때문에 반복문 정지
-                if(remainder != 0 && turn == quotient - 1) {  // 마지막 시행 (나머지만큼)
-                        indicator = bubbleSort(arr, turn * 5, (turn + 1) * 5 + remainder, n, k, bw);
+                if (indicator) {
+                    break;
+                } // 시행이 끝나기 이전에 swapNo == k 만족시 true를 반환하기 때문에 반복문 정지
+                if (remainder != 0 && turn == quotient - 1) {  // 마지막 시행 (나머지만큼)
+                    indicator = bubbleSort(arr, turn * 5, (turn + 1) * 5 + remainder, n, k);
                     break; // 정지 (나머지 0인 경우 조건문 패스하고 바로 밑의 줄 실행하게 됨 바로 아래줄 한번 더 실행)
                 }
-                    indicator = bubbleSort(arr, turn * 5, (turn + 1) * 5, n,  k, bw);
+                indicator = bubbleSort(arr, turn * 5, (turn + 1) * 5, n, k);
             }
         }
 
-        if(!indicator){ // 위의 bubbleSort 실행과정에서 최종적으로 swapNo == k를 만족하지 않게 되는 경우, indicator = false는 변화 없음.
+        if (!indicator) { // 위의 bubbleSort 실행과정에서 최종적으로 swapNo == k를 만족하지 않게 되는 경우, indicator = false는 변화 없음.
             bw.write("-1");
+        } else {
+            printArray(bw);
         }
 
         bw.flush();
@@ -55,35 +60,34 @@ public class BubbleSort_4_24047 {
     }
 
 
-    static boolean bubbleSort(int[] target, int start, int end, int n , int k, BufferedWriter bw) throws IOException {
+    static boolean bubbleSort(int[] target, int start, int end, int n, int k) throws IOException {
+        BufferedWriter bf = new BufferedWriter(new OutputStreamWriter(System.out));
         for (int i = start; i < n - 1; i++) {
-            for (int j = start; j < n - i - 1 ; j++) {
-                if (arr[j] > arr[j+1]) {
+            for (int j = start; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
                     ++swapNo;
                     int temp = target[j];
-                    target[j] = target[j+1];
-                    target[j+1] = temp;
-//                    bw.write(swapNo + "\n");
-//                    printArray(bw);
-                    if(swapNo == k){
-                        printArray(bw);
-                        return true;
-                    }
+                    target[j] = target[j + 1];
+                    target[j + 1] = temp;
+                    printArray(bf);
+//                    if(swapNo == k){
+//                        return true;
+//                    }
 
                 }
             }
         }
+        bf.flush();
+        bf.close();
         return false;
     }
 
-    static void printArray(BufferedWriter bw) throws IOException{
+    static void printArray(BufferedWriter bw) throws IOException {
         for (int i : arr) {
             bw.write(Integer.toString(i));
             bw.write(" ");
         }
         bw.write("\n");
     }
-
-
 
 }
---
+++ src/Baekjoon/Algorithm_Class/SelectionSort_1_23881.java
@@ -0,0 +1,80 @@
+package Baekjoon.Algorithm_Class;
+
+/*
+N개의 서로 다른 양의 정수가 저장된 배열 A가 있다.
+선택 정렬로 배열 A를 오름차순 정렬할 경우 K 번째 교환되는 수를 구해서 우리 서준이를 도와주자.
+크기가 N인 배열에 대한 선택 정렬 의사 코드는 다음과 같다.
+ */
+
+import java.io.*;
+import java.nio.Buffer;
+import java.util.StringTokenizer;
+
+public class SelectionSort_1_23881 {
+
+    static int n;
+    static int k;
+    static int[] target;
+
+    static int swapNo = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        target = new int[n];
+        k = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine(), " ");
+        // retrieving array values
+        for(int i = 0; i<n; i++){
+            target[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] result = selectionSort();
+        printArray(result, bw);
+
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+    static int[] selectionSort() throws IOException {
+        int i, j;
+        int max;
+
+        for(i= target.length - 1; i > 0; i--){
+            max = i;
+            for(j=i - 1; j >= 0; j--){
+                if(target[max] < target[j]){
+                    max = j;
+                }
+            }
+            if(i != max) {
+                swap(target, i, max);
+                swapNo++;
+            }
+            if(swapNo == k){
+                return new int[]{target[max], target[i]};
+            }
+        }
+        return new int[]{-1};
+    }
+
+    // target array to swap, index1, index2
+    static void swap(int[] target, int a, int b){
+        int temp = target[a];
+        target[a] = target[b];
+        target[b] = temp;
+    }
+
+    static void printArray(int[] target, BufferedWriter bw) throws IOException{
+        for(int i = 0; i < target.length; i++){
+            bw.write(Integer.toString(target[i]));
+            bw.write(" ");
+        }
+        bw.write("\n");
+    }
+}
